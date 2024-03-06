6713293d76ae13f4fb54cb98415c633acc005f03
Rishikesh <rishikesh2511990@gmail.com>
2024-03-06 14:41:15 +0530

    msg

---
+++ ex4/src/App.jsx
@@ -0,0 +1,16 @@
+
+import Gall from './components/Gallery.jsx'
+import {Profile} from './components/Gallery.jsx';
+
+function App() {
+
+  return (
+    <>
+      <Gall/>
+      <h2>hello</h2>
+      <Profile></Profile>
+    </>
+  )
+}
+
+export default App
